d84af24387e53f9f57e27292e1f2440a8d1c2e33
RukudzoNdlovu <rukudzondlovu@gmail.com>
2020-10-15 08:03:00 +0200

    Update my_extras.py

--- learning_templates/basic_app/templatetags/my_extras.py
+++ learning_templates/basic_app/templatetags/my_extras.py
@@ -8,6 +8,6 @@ def cut(value,arg):
     """
     this cuts out all values of arg from the string
     """
-    return value.replace(arg. '')
+    return value.replace(arg, '')
 
     # register.filter('cut',cut)
